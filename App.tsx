4d3a557f4583a09e466b378d4b44b520eed6c878
Denis Dolbilin <kombogame6@gmail.com>
2023-07-15 19:03:45 +0300

    feat: added Raleway fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  useFonts,
+  Raleway_400Regular,
+  Raleway_500Medium,
+  Raleway_600SemiBold,
+} from '@expo-google-fonts/raleway';
 
 import Navigator from 'navigation/RootNavigation';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Raleway_400Regular,
+    Raleway_500Medium,
+    Raleway_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={styles.gestureRoot}>
