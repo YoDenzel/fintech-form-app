673b8407d03a6b8a64e7b1db1d3e71a146bb55f1
Denis Dolbilin <kombogame6@gmail.com>
2023-07-15 14:53:11 +0300

    feat: created background gradient

--- src/screens/BookASlot/BookASlot.tsx
+++ src/screens/BookASlot/BookASlot.tsx
@@ -1,5 +1,20 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import styles from './styles';
 
 export const BookASlot = () => {
-        return <View></View>;
-};  
+  const { top } = useSafeAreaInsets();
+  return (
+    <View style={[styles.screen, { paddingTop: top }]}>
+      <LinearGradient
+        colors={['rgba(246, 247, 250, 0.2)', 'rgba(66, 63, 255, 0.1)']}
+        start={{ x: 0.1, y: 0.3 }}
+        end={{ x: 1, y: 0.3 }}
+        style={styles.linearGradient}>
+        <Text>Book a slots</Text>
+      </LinearGradient>
+    </View>
+  );
+};
---
+++ src/screens/BookASlot/index.ts
@@ -0,0 +1,1 @@
+export { BookASlot } from './BookASlot';
