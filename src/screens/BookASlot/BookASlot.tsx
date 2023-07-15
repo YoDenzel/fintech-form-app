ebf45551f0f380f4593a996805cee8eadc3a9ffb
Denis Dolbilin <kombogame6@gmail.com>
2023-07-15 19:24:18 +0300

    feat: added title and submit button

--- src/screens/BookASlot/BookASlot.tsx
+++ src/screens/BookASlot/BookASlot.tsx
@@ -1,19 +1,40 @@
 import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
+import { ButtonDefault } from 'components/ButtonDefault/ButtonDefault';
+import ScreenHeader from 'components/ScreenHeader/ScreenHeader';
 
 import styles from './styles';
+import { Checkbox } from 'components/Checkbox';
 
 export const BookASlot = () => {
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
   return (
     <View style={[styles.screen, { paddingTop: top }]}>
+      <StatusBar />
       <LinearGradient
         colors={['rgba(246, 247, 250, 0.2)', 'rgba(66, 63, 255, 0.1)']}
-        start={{ x: 0.1, y: 0.3 }}
-        end={{ x: 1, y: 0.3 }}
-        style={styles.linearGradient}>
-        <Text>Book a slots</Text>
+        start={{ x: 0.1, y: 0.4 }}
+        end={{ x: 1, y: 0.8 }}
+        style={[styles.linearGradient, { paddingBottom: bottom + 20 }]}>
+        <ScreenHeader onBack={() => undefined} />
+        <View style={styles.pageTitleContainer}>
+          <Text style={styles.pageTitle}>Забронировать слот</Text>
+          <Text style={styles.pageTitleDescription}>
+            Оставьте контактные данные, и мы с вами свяжемся в ближайший час.
+          </Text>
+        </View>
+        <View>
+          <ButtonDefault
+            text="Отправить"
+            onPress={() => undefined}
+            style={styles.submitButton}
+          />
+          <View>
+            <Checkbox />
+          </View>
+        </View>
       </LinearGradient>
     </View>
   );
--- src/screens/BookASlot/styles.ts
+++ src/screens/BookASlot/styles.ts
@@ -3,8 +3,32 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   screen: {
     flex: 1,
+    backgroundColor: 'rgba(246, 247, 250, 0.2)',
   },
   linearGradient: {
     flex: 1,
+    paddingHorizontal: 20,
+  },
+  pageTitleContainer: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  pageTitle: {
+    textAlign: 'center',
+    fontFamily: 'Raleway_600SemiBold',
+    fontSize: 24,
+    lineHeight: 34,
+    color: '#1E1E20',
+    marginBottom: 8,
+  },
+  pageTitleDescription: {
+    textAlign: 'center',
+    fontFamily: 'Raleway_400Regular',
+    fontSize: 15,
+    lineHeight: 21,
+    color: '#60626D',
+  },
+  submitButton: {
+    marginBottom: 10,
   },
 });
